423e3934ae5c7c43afe9cbebf2a25a7aa1c35113
Ashutosh Kumar <ashutosh.kr1818@gmail.com>
2026-05-29 16:19:03 +0530

    update

--- langchain/ollama/app.py
+++ langchain/ollama/app.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-from langchain_community.chat_models import ChatOllama
+from langchain_community.chat_models import ChatOllama 
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import streamlit as st
